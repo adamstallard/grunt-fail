1917b9eeb3822fd55879efe3fd07cdf0ccba89d3
adamstallard <adam@goalzen.org>
2013-03-07 19:00:28 -0800

    first version

---
+++ tasks/fail.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(grunt){
+  grunt.registerTask('fail', 'Cause grunt to fail with a message and/or return code', function(){
+    grunt.verbose.subhead('fail:arguments');
+    grunt.verbose.writeflags(arguments);
+    var message = 'Failed on purpose.';
+    var code = -1;
+    if (isNaN(arguments[0])) {
+      if (arguments.length) {
+        message = arguments[0];
+      }
+      if (arguments.length > 1) {
+        code = parseInt(arguments[1]);
+      }
+    }
+    else {
+      if (arguments.length) {
+        code = parseInt(arguments[0]);
+      }
+    }
+    grunt.fail.fatal(message, code);
+  });
+};
---
+++ test/grunt.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var vows = require('vows');
+var exec = require('child_process').exec;
+var expect = require('chai').expect;
+var grunt = require('grunt');
+
+vows.describe('grunt').addBatch({
+  "grunt fail:25" : {
+    topic : function(){
+      exec('grunt --verbose fail:25', this.callback);
+    },
+    "should have an error code of 25" : function(error, stdout){
+      grunt.verbose.error(error);
+      grunt.verbose.error().errorlns(stdout);
+      expect(error).to.have.property('code', 25);
+    }
+  },
+  "grunt fail:awesome" : {
+    topic : function(){
+      exec('grunt --verbose fail:awesome', this.callback);
+    },
+    "should have an error message of 'awesome'" : function(error, stdout){
+      grunt.verbose.error(error);
+      grunt.verbose.error().errorlns(stdout);
+      expect(stdout).to.have.string('error: awesome');
+    }
+  },
+  "grunt fail:awesome:25" : {
+    topic : function(){
+      exec('grunt --verbose fail:awesome:25', this.callback);
+    },
+    "should have an error message of 'awesome'" : function(error, stdout){
+      expect(stdout).to.have.string('error: awesome');
+    },
+    "should have an error code of 25" : function(error){
+      expect(error).to.have.property('code', 25);
+    }
+  },
+  "grunt fail" : {
+    topic : function(){
+      exec('grunt --verbose fail', this.callback);
+    },
+    "should have an error message of 'Failed on purpose.'" : function(error, stdout){
+      grunt.verbose.error(error);
+      grunt.verbose.error().errorlns(stdout);
+      expect(stdout).to.have.string('error: Failed on purpose');
+    },
+    "should have an error code of -1" : function(error){
+      expect(error).to.have.property('code', -1);
+    }
+  }
+}).export(module, {error: false});
